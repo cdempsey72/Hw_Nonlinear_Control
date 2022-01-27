1930491e20b40670dff01923e117a8db3d5d4f2d
cdempsey72 <dempsey.c72@gmail.com>
2022-01-26 20:16:12 -0600

    Corrected van der pol simulation

--- Hw1_ME8352.m
+++ Hw1_ME8352.m
@@ -36,32 +36,45 @@ title('Position v. Time')
 %% Problem 2: m*x_ddot + 2*c*(x^2 - 1)*x_dot + k*x = 0
 clear; close all; clc;
 %%Define initial parameters and arrays
-x0_array = 0:0.1:1; 
-xdot0_array = -1:0.1:1;
-t_sim = 3; %set simulation time
-m = 1; %mass
-k = 1; %stiffness coefficient
-c = 1; %damping coefficient
-x_t = [];
-x_dot = [];
+x0_array = -3:1:3; 
+xdot0_array =0;
+t_sim = 25; %set simulation time
+m = 1;      %mass
+k = 2;      %stiffness coefficient
+c = 3;     %damping coefficient
 
 %%Simulate
+f1 = figure;
+f2 = figure;
+hold on
 for i = 1:length(x0_array)
+    %update initial condition
+    x_0 = x0_array(i); 
     for ii = 1:length(xdot0_array)
         %update initial condition
         x_dot0 = xdot0_array(ii); 
         %call simulink model
         states = sim('Hw1_ME8352sim.slx'); 
-        %extract states and store in an array
-        x_t(ii,:) = states.x_pos.Data(:,1);
-        x_dot(ii,:) = states.x_dot.Data(:,1);
+        %extract states
+        t_out = states.tout; %time stamps
+        x_t = states.x_pos.Data(:,1);
+        x_dot = states.x_dot.Data(:,1);
+        %plot phase plane
+        figure(f1);
+        hold on
+        plot(x_t,x_dot)
+        %plot time-based evolution
+        figure(f2);
+        hold on
+        plot(t_out,x_t)
     end
 end
-t_out = states.tout; %extract simulation time stamps
-
-%%Plot state behavior
-figure
-plot(x_t,x_dot)
+hold off
+%label figures
+figure(f1);
+title('Phase Plane'); xlabel('x_t'); ylabel('x_dot')
+figure(f2);
+title('x v. time'); xlabel('time (s)'); ylabel('x_t')
 
 %% Problem 3: v_dot = u - |v|*v
 clear; close all; clc;
