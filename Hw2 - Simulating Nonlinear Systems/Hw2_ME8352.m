39432bdd837d22900b150424c764943647cf8ee8
cdempsey72 <dempsey.c72@gmail.com>
2022-02-06 22:21:02 -0600

    chaotic behavior now seen by adjusting forcing function

--- Hw2 - Simulating Nonlinear Systems/Hw2_ME8352.m
+++ Hw2 - Simulating Nonlinear Systems/Hw2_ME8352.m
@@ -3,8 +3,11 @@
 %% Problem 1 - Explore Duffing Equation
 clear; clc; close all;
 %%Define initial parameters and variables
-b_ = 0; %damping coefficient
-a_ = 1; %linear stiffness coefficient
+b_ = 0.3; %damping coefficient
+a_ = -1; %linear stiffness coefficient
 beta_ = 1; %nonlinear stiffness coefficient
-x_0 = []; %initial state condition
-xdot_0 = []; %initial state condition
+omega_ = 1.2;
+gamma_ = 0.37;
+x_0 = 1; %initial state condition
+xdot_0 = 0; %initial state condition
+t_sim = 30; %set simulation time
