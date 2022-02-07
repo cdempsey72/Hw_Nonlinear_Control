5629946f4038133ab37e11772d5e6107f8c1f618
cdempsey72 <dempsey.c72@gmail.com>
2022-02-06 21:51:48 -0600

    Initial problem 1 setup

---
+++ Hw2 - Simulating Nonlinear Systems/Hw2_ME8352.m
@@ -0,0 +1,10 @@
+%%% ME8352 Homework 2 %%%
+
+%% Problem 1 - Explore Duffing Equation
+clear; clc; close all;
+%%Define initial parameters and variables
+b_ = 0; %damping coefficient
+a_ = 1; %linear stiffness coefficient
+beta_ = 1; %nonlinear stiffness coefficient
+x_0 = []; %initial state condition
+xdot_0 = []; %initial state condition
