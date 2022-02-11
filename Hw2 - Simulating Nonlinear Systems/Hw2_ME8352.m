d8622aad38a4b98488dfc2337c79e223a4af7d57
cdempsey72 <dempsey.c72@gmail.com>
2022-02-11 15:23:47 -0600

    Fixed simulink model of the satellite control system example; now shows proper stability and switching behavior. Removed problem 4 material; became easier to sketch and evaluate by hand than solving an ode.

--- Hw2 - Simulating Nonlinear Systems/Hw2_ME8352.m
+++ Hw2 - Simulating Nonlinear Systems/Hw2_ME8352.m
@@ -48,14 +48,13 @@ title('x v. time'); xlabel('time (s)'); ylabel('x_t')
 clear; clc; close all;
 %%Define initial parameters and variables
 a_ = 1;     %switching line coefficient
-U_ = 1;     %control input
 
-th_0 = 0;    %initial state condition
-th_dot0 = 2; %initial state condition
+th_0 = 1;    %initial state condition
+th_dot0 = 0.5; %initial state condition
 t_sim = 10; %set simulation time
 
-% th0_array = linspace(0,2,10);
-th0_array = pi/6;
+th0_array = linspace(0,pi/4,10);
+% th0_array = pi/6;
 
 %%Simulate
 f1 = figure; %phase plot figure
@@ -78,47 +77,6 @@ for i = 1:length(th0_array)
     figure(f2);
     hold on
     plot(t_out,th)
-end
-hold off
-%label figures
-figure(f1);
-title('Phase Plane'); xlabel('th'); ylabel('th_{dot}')
-figure(f2);
-title('theta v. time'); xlabel('time (s)'); ylabel('theta')
-
-%% Problem 4
-clear; clc; close all;
-%%Define initial parameters and variables
-alpha_ = pi/16;      %switching line coefficient
-length_a = 9.81; %control input
-g = 9.81;        %gravitational constant
-th_0 = 0;        %initial state condition
-th_dot0 = 0;     %initial state condition
-t_sim = 10;      %set simulation time
-
-th0_array = linspace(0,pi/4,10);
-
-%%Simulate
-f1 = figure; %phase plot figure
-f2 = figure; %time plot figure
-hold on
-for i = 1:length(th0_array)
-    %update initial condition
-    th_0 = th0_array(i);
-        %call simulink model
-        states = sim('Hw2_ME8352sim.slx'); 
-        %extract states
-        t_out = states.tout; %time stamps
-        th = states.th.Data(:,1);
-        th_dot = states.th_dot.Data(:,1);
-        %plot phase plane
-        figure(f1);
-        hold on
-        plot(th,th_dot)
-        %plot time-based evolution
-        figure(f2);
-        hold on
-        plot(t_out,th)
 end
 hold off
 %label figures
